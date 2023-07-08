b142fb3b072a4c53b422ca4e01ff45eb88dc38c8
colins-uky <colin.schuh@uky.edu>
2023-07-07 21:47:33 -0400

    Started color selector chrome extension.

---
+++ scripts/popup.js
@@ -0,0 +1,43 @@
+const colorPickerButton = document.querySelector("#pick-color");
+const colorContainer = document.querySelector(".color-container");
+const color1 = document.querySelector(".color-1");
+
+const hexToRGB = (hex) => {
+    let r = 0, g = 0, b = 0;
+
+    // 3 digits
+    if (hex.length == 4) {
+        r = "0x" + hex[1] + hex[1];
+        g = "0x" + hex[2] + hex[2];
+        b = "0x" + hex[3] + hex[3];
+
+    // 6 digits
+    } else if (hex.length == 7) {
+        r = "0x" + hex[1] + hex[2];
+        g = "0x" + hex[3] + hex[4];
+        b = "0x" + hex[5] + hex[6];
+    }
+    
+    return "rgb("+ +r + "," + +g + "," + +b + ")";
+}
+
+
+const activateEyeDropper = async () => {
+    try {
+        const eyeDropper = new EyeDropper();
+        const response = await eyeDropper.open();
+        const hex = response.sRGBHex;
+        const rgb = hexToRGB(hex);
+
+
+        console.log(hex);
+        console.log(rgb);
+
+        color1.style.backgroundColor = rgb;
+        colorContainer.style.visibility = "visible";
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+colorPickerButton.addEventListener("click", activateEyeDropper);
